cb854484b31fed5a1354ea64f34ffe00ee929dfe
LonerYang <1051233525@qq.com>
2019-04-17 22:55:27 +0800

    删除了src

--- src/main/webapp/resource/js/superadmin/superadminmanage.js
+++
@@ -1,5 +0,0 @@
-$(function() {
-    $("#shop-manage").prop("href", "/o2o/superAdmin/shopListPage");
-    $("#local-auth-manage").prop("href", "/o2o/superAdmin/userListPage");
-    $("#update-password").prop("href", "/o2o/superAdmin/modifyPasswordPage");
-})
